dfef2a206cc44b43ffd243e8bf07f8582d7c72bf
ZurdenRo <rodrigo.nievarobles@gmail.com>
2021-10-01 14:43:36 -0300

    Se modifico la clase main, del cual corre el programa

--- src/development/game/app/App.java
+++ src/development/game/app/App.java
@@ -3,18 +3,31 @@ package development.game.app;
 
 import development.game.model.Cell;
 import development.game.model.Cuadricula;
+import development.game.model.Tablero;
 
 
-public class App {
-	public static void main(String[] args) {
+public class App{
 
-		  	Cuadricula c = new Cuadricula();
-	        Cell[] [] cellsOne =  new Cell[3] [3];
-	        c.createMatrixNumber(cellsOne);
-	        Cuadricula.fillNumbersAvailable();
-	        c.removeNumberRepetitive(cellsOne);
-	        c.markTrueNumberRepetitive(cellsOne);
-	        c.printMatrix(cellsOne);
-	}
+    public static void main(String[] args){
+
+        Cuadricula c = new Cuadricula();;
+        Cell[][] cellsOne = new Cell[3][3];
+        c.createMatrixNumber(cellsOne);
+        Cuadricula.fillNumbersAvailable();
+        c.removeNumberRepetitive(cellsOne);
+        c.markTrueNumberRepetitive(cellsOne);
+        c.printMatrix(cellsOne);
+
+        Cell[][] cellsTwo = new Cell[3][3];
+        c.createMatrixNumber(cellsTwo);
+        Cuadricula.fillNumbersAvailable();
+        c.removeNumberRepetitive(cellsTwo);
+        c.markTrueNumberRepetitive(cellsTwo);
+        c.printMatrix(cellsTwo);
+
+        Tablero tablero = new Tablero();
+        tablero.setGridOnTable(cellsOne, cellsTwo);
+        //t.printGrid();
+    }
 
 }
