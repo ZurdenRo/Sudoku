069e48a038588fa78ad052d4a1cb39413fe8da93
ZurdenRo <rodrigo.nievarobles@gmail.com>
2021-10-05 18:10:09 -0300

    Se toma en cuenta las posiciones de las celdas y las grillas de manera independiente

--- src/development/game/model/PositionGrid.java
+++ src/development/game/model/PositionGrid.java
@@ -2,4 +2,46 @@ package development.game.model;
 
 public class PositionGrid{
 
+    private Cell cell;
+    private String idGrid;
+    private Position positionGrid;
+
+    public PositionGrid(String idGrid, Cell cell, Position positionGrid){
+        setIdGrid(idGrid);
+        setCell(cell);
+        setPositionGrid(positionGrid);
+    }
+
+    public Position getPositionGrid(){
+        return positionGrid;
+    }
+
+    public void setPositionGrid(Position positionGrid){
+        this.positionGrid = positionGrid;
+    }
+
+    public Cell getCell(){
+        return cell;
+    }
+
+    public void setCell(Cell cell){
+        this.cell = cell;
+    }
+
+    public String getIdGrid(){
+        return idGrid;
+    }
+
+    public void setIdGrid(String idGrid){
+        this.idGrid = idGrid;
+    }
+
+    @Override
+    public String toString(){
+        return "PositionGrid{" +
+                "cell=" + cell +
+                ", idGrid='" + idGrid + '\'' +
+                ", p=" + positionGrid +
+                '}';
+    }
 }
