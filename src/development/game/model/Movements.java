9b95052ed9f0ea013ba4d351027bf67b7914058a
ZurdenRo <rodrigo.nievarobles@gmail.com>
2021-10-14 22:02:45 -0300

    Se agrego al enum una nueva forma de busqueda

--- src/development/game/model/Movements.java
+++ src/development/game/model/Movements.java
@@ -2,13 +2,45 @@ package development.game.model;
 
 public enum Movements{
 
-    LEFT(-1), RIGHT(1), DOWN(-1), UP(1);
+    LEFT(-1, PositionMovements.HORIZONTAL),
+    RIGHT(1, PositionMovements.HORIZONTAL),
+    DOWN(-1, PositionMovements.VERTICAL),
+    UP(1, PositionMovements.VERTICAL);
 
     private int number;
+    private PositionMovements positionMovements;
 
-    Movements(int number){ }
-
-    public int getMovements(){
-        return this.number;
+    Movements(int number, PositionMovements positionMovements){
+        setNumber(number);
+        setPositionMovements(positionMovements);
     }
+
+    public void setNumber(int number){
+        this.number = number;
+    }
+
+    public void setPositionMovements(PositionMovements positionMovements){
+        this.positionMovements = positionMovements;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public PositionMovements getPositionMovements(){
+        return positionMovements;
+    }
+
+    private enum PositionMovements{
+        VERTICAL("VERTICAL"), HORIZONTAL("HORIZONTAL");
+
+        private String posMovements;
+
+        PositionMovements(String posMovements){}
+
+        public String getPosMovements(){
+            return this.getPosMovements();
+        }
+    }
+
 }
