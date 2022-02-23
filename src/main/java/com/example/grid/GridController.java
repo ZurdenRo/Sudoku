ecf01cd5028af8ce939fe5c903809d2adc013471
ZurdenRo <rodrigo.nievarobles@gmail.com>
2022-02-23 17:30:04 -0300

    modify controller

--- src/main/java/com/example/grid/GridController.java
+++ src/main/java/com/example/grid/GridController.java
@@ -1,5 +1,21 @@
 package com.example.grid;
 
+
+
+import com.example.game.designPattern.factory.GridFactory;
+import com.example.game.designPattern.factory.IGrid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/grid")
 public class GridController{
+
+
+    @GetMapping
+    public IGrid hello(){
+        return GridFactory.createGridThree();
+    }
 
 }
